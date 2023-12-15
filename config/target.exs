84cf7dcc9eeba2283267482d74b06db0527173d0
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-12-15 09:24:43 +0900

    Change SSH key name for this project

--- config/target.exs
+++ config/target.exs
@@ -26,9 +26,7 @@ config :nerves, :erlinit, update_clock: true
 
 keys =
   [
-    Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
+    Path.join([System.user_home!(), ".ssh", "zybo_z7_10_demo_rsa.pub"])
   ]
   |> Enum.filter(&File.exists?/1)
 
